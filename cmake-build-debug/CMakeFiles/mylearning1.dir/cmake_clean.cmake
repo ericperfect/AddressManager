24260862855332f472cd9882541a9bed9be978cc
Eric <ericperfectttt@gmail.com>
2020-11-16 09:11:38 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/mylearning1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mylearning1.dir/main.cpp.o"
+  "mylearning1"
+  "mylearning1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mylearning1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
